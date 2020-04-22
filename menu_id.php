b349782da768da9d782864963f5965cd07f0c7d4
fabrice ivaldi <fabrice.ivaldi@gmail.com>
2020-04-22 12:38:19 +0200

    Add files via upload

---
+++ menu_id.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="stylesheets/menuid.css" />
+
+<div class="menuidz100">
+    <ul>
+        <li class="compte"><img src="images/id.png"/>&nbsp;fabdev</a>
+            <div class="submenu">
+                <ul>
+                    <li class="sousmenucompte"><a href="#">Mon compte</a></li>
+                        <li class="detail"><img src="images/cadenas.png"/>&nbsp;Mon abonnemment</li>
+                        <li class="detail"><img src="images/idsmall.png"/>&nbsp;Mes informations &nbsp;&nbsp;&nbsp;&nbsp; personnelles</li>
+                        <li class="detail"><img src="images/idsmall.png"/>&nbsp;Mes avantages abonné</li>
+                    <li class="sousmenus"><a href="#">Espace personnel</a></li>
+                        <li class="detail"><img src="images/coeur.png"/>&nbsp;Mes favoris</li>
+                        <li class="detail"><img src="images/comments.png"/>&nbsp;Mes commentaires</li>
+                        <li class="detail"><img src="images/contacts.png"/>&nbsp;Mes contacts</li>
+                        <li class="detail"><img src="images/message.png"/>&nbsp;Mes messages</li>
+                    <li class="sousmenus"><a href="#">Profil public</a></li>
+                    <li class="sousmenudelog"><a href="#"><img src="images/delog.png"/>&nbsp;Se déconnecter</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div>  
